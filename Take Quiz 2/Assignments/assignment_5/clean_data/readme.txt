bf258875a3c1a3fa65dba0c5c88cf210b4a2f1fc
jillianpkoenig <jillian.koenig@emory.edu>
2025-10-18 11:45:56 -0400

    updating

--- Take Quiz 2/Assignments/assignment_5/clean_data/readme.txt
+++
@@ -1,2 +0,0 @@
-Folder currently empty.
-You will move one or more files here for HW #5
--- Take Quiz 2/Assignments/assignment_5/scripts/auxiliaryfunction.py
+++
@@ -1,5 +0,0 @@
-def leagues_to_miles(L):
-    # Convert metric leagues to miles
-    km = 4*L
-    miles = km/1.609344
-    return miles
